02af7ce6d5927ff7afb98d30f7d89caf2da1a3f4
Adam Yarrow <adam.yarrow@colorado.edu>
2026-04-03 14:41:43 -0600

    Imperial units and inconsistent papers are making me cry.

--- ModelParams.m
+++ ModelParams.m
@@ -1,5 +1,6 @@
 function [const] = ModelParams(varargin)
     FEET_TO_M = 0.3048;
+    LB_TO_KG = 0.453;
 
     const = struct();
     
@@ -13,17 +14,62 @@ function [const] = ModelParams(varargin)
     const.g = 9.81; % m/s^2
 
     % TODO - update these with paper values
+    %{
+        NOTE: using "Control Orientated Modelling of an Air-Breathing
+        Hypersonic Vehicle" by Parker, Bolender and Doman as the master
+        source of truth for simulation parameters. 
+
+        Assuming when any units are normalized by /ft it means they are
+        scaled by the vehicle length.
+
+        Assuming that the canard is identical to the elevator in lift/drag.
+    %}
+
     vehicleLength = 100*FEET_TO_M; % 100 ft to m
-    const.gravimetrics.m = 4377*vehicleLength; % kg (300 slugs/ft???)
+    const.gravimetrics.m = 4377*vehicleLength; % kg (300 lb/ft - assuming normalized by length?)
     const.gravimetrics.Iyy = 5E5*1.35581795*vehicleLength; % kg.m^2 (5E5 slugs.ft^2/ft)
 
-    const.thrust.zOffset = 2.548;
+    % Thrust Params
+    const.thrust.zOffset = 2.548; % m
+    beta(1) = -3.7693e5;  % lb·ft^-1·rad^-3
+    beta(2) = -3.7225e4;  % lb·ft^-1·rad^-3
+    beta(3) =  2.6814e4;  % lb·ft^-1·rad^-2
+    beta(4) = -1.7277e4;  % lb·ft^-1·rad^-2
+    beta(5) =  3.5542e4;  % lb·ft^-1·rad^-1
+    beta(6) = -2.4216e3;  % lb·ft^-1·rad^-1
+    beta(7) =  6.3785e3;  % lb·ft^-1
+    beta(8) = -1.0090e2;  % lb·ft^-1
+    const.thrust.beta = beta ;
 
-    const.aero.refArea = (17*FEET_TO_M^2)/FEET_TO_M * vehicleLength;
-    const.aero.refLength = 5.1816;
+    % Aero Coeffs
+    const.aero.refArea = (17*FEET_TO_M^2)/FEET_TO_M * vehicleLength; % 17ft^2/ft normalized to vehicle length
+    const.aero.refLength = 17*FEET_TO_M; % m 
+
+    % NOTE: assuming the canard is identical to the elevator in terms of
+    % size
+    kec = -1.5; % Scaling factor for moments in deltaE vs deltaC.
+    % NOTE: I bastardised the paper "Control Orientated Modelling of
+    % Air-Breathing Hypersonic Vehicles" by Parker et al.
     const.aero.CL_alpha = 4.6773; % rad^-1
     const.aero.CL_deltaE = 7.6224E-1; % rad^-1
-    const.aero.CL_deltaC = ???; % Do we just make this negative of deltaE?
+    const.aero.CL_deltaC = const.aero.CL_deltaE; % Assuming canard == elevator
+    const.aero.CL_0 = -1.8714E-2;
+
+    const.aero.CD_alphaSq = 5.8224; % rad^-2
+    const.aero.CD_alpha = -4.5315E-2; % rad^-1
+    const.aero.CD_deltaESq = 8.1993E-1; % rad^-2
+    const.aero.CD_deltaE = 2.7699E-4; % rad^-1
+    const.aero.CD_deltaCSq = const.aero.CD_deltaESq; % rad^-2
+    const.aero.CD_deltaC = const.aero.CD_deltaE; % rad^-1
+    const.aero.CD_0 = 1.0131E-2; 
+
+    const.aero.CM_alphaSq = 6.2926; % rad^-2
+    const.aero.CM_alpha = 2.1335; % rad^-1
+    const.aero.CM_0 = 1.8979E-1;
+    const.aero.CM_deltaE = -1.2897; % rad^-1
+    const.aero.CM_deltaC = kec * const.aero.CM_deltaE;
+
+
     
 
 
