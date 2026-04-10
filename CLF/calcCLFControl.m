54116779af7ec1f74781ff1d7a110da57a1ec5c3
etacarinae2300-admin <suryahyma@gmail.com>
2026-04-09 20:38:17 -0600

    CLF Bug Fixes

--- CLF/calcCLFControl.m
+++ CLF/calcCLFControl.m
@@ -1,4 +1,4 @@
-function u = calcCLFControl(x, x_r, xDot_r, P, const)
+function [u, temp] = calcCLFControl(x, x_r, xDot_r, P, const)
     % Get the Lie Derivatives
     [LfV, LgV] = calcCLFLieDerivs(x, x_r, xDot_r, P, const);
     z = x(1:4) - x_r(1:4);
@@ -8,25 +8,37 @@ function u = calcCLFControl(x, x_r, xDot_r, P, const)
     V = z' * P * z;
     unknown_term = 0; % Unknown Term in Eq 15
     % Replace Unknown Term later with : abs(grad_V * G * l_sq * W)
+    temp = [];
     
-    % Solving by QP
-    % QP minimizes (0.5*x'*H*x) + f'*x
-    % For our problem, we minimize u'*u, So H = 2*I & f' = 0
-    H = 2 * eye(3); 
-    f = zeros(3, 1);
-    
-    % QP requires the constraint to be of the form A*x </= b
-    A = LgV; 
-    b = -LfV - epsilon * V - unknown_term;
-    
-    lb = [0; -deg2rad(30); -deg2rad(30)]; % Minimums [phi, delta_e, delta_c]
-    ub = [1.2; deg2rad(30); deg2rad(30)]; % Maximums
-    
-    options = optimoptions('quadprog', 'Display', 'off');
-    u = quadprog(H, f, A, b, [], [], lb, ub, [], options);
-    
+    %% Solving by QP
+    % % QP minimizes (0.5*x'*H*x) + f'*x
+    % % For our problem, we minimize u'*u, So H = 2*I & f' = 0
+    % H = 2 * eye(3); 
+    % f = zeros(3, 1);
+    % 
+    % % QP requires the constraint to be of the form A*x </= b
+    % A = LgV; 
+    % b = -LfV - epsilon * V - unknown_term;
+    % 
+    % lb = [0; -deg2rad(30); -deg2rad(30)]; % Minimums [phi, delta_e, delta_c]
+    % ub = [1.2; deg2rad(30); deg2rad(30)]; % Maximums
+    % 
+    % options = optimoptions('quadprog', 'Display', 'off');
+    % u = quadprog(H, f, A, b, [], [], lb, ub, [], options);
+    % 
     % If QP is completely infeasible with bounds, fallback to zero effort
-    if isempty(u)
-        u = [0; 0; 0]; 
+    % if isempty(u)
+    %     u = [0; 0; 0]; 
+    % end
+
+    %% Solving by PMN Method
+
+    psi0 = LfV + epsilon * V;  % Eq. (12)
+    psi1 = LgV';   
+
+    if psi0 <= 0
+        u = zeros(3, 1);          
+    else
+        u = -((psi0 * psi1) / (psi1' * psi1));  
     end
 end
