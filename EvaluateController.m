d249f65a3ef2a4466f99771d30e528faf01012b9
Adam Yarrow <adam.yarrow@colorado.edu>
2026-04-22 10:31:14 -0600

    Cleanup for submission.

--- EvaluateController.m
+++ EvaluateController.m
@@ -29,26 +29,5 @@ simData_CBFoff = Simulation(xIC, clfCbfKKT, tEnd);
 plotSimulationResults({simData_CBFon, simData_CBFoff}, {'CBF ON','CBF OFF'},@getRefTraj, P);
 
 
-% %% No Control - Testing Open Loop Response
-% xIC_Regulation = [2077;
-%                   0;
-%                   0;
-%                   0;
-%                   25908];
-% tEnd = 10;
-% 
-% simData_CBF = Simulation(xIC_Regulation, @CBFControl,tEnd);
-% plotSimulationResults(simData_CBF,'CBF Controller');
-% 
-% simData_OL = Simulation(xIC_Regulation, @OpenLoopControl,tEnd);
-% plotSimulationResults(simData_OL,'OL Controller');
-% 
-% % PID
-% simData_PID = Simulation(xIC_Regulation, @PID,tEnd);
-% plotSimulationResults(simData_PID,'PID on Pitch Rate Controller');
-
-
-
-
 
 
